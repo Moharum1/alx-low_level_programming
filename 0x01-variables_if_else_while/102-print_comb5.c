9f1ef5e1f88959663f1f681f5dd0d48f6ad41115
Moharum1 <moharum4@gmail.com>
2023-03-18 10:22:15 -0700

    Hi

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,18 +4,24 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main() {
+int num1, num2;
+for(num1 = 0; num1 <= 99; num1++)
 {
-int i, j;
-for (i = 0; i < 9; i++)
+for(num2 = num1; num2 <= 99; num2++)
 {
-for (j = 0; j <= 9; j++)
+if(num1 != num2)
 {
-putchar(i + '0');
-putchar(j + '0');
-if (i != 8 || j != 9) {
+putchar(num1 / 10 + '0');
+putchar(num1 % 10 + '0');
+putchar(' ');              
+putchar(num2 / 10 + '0');
+putchar(num2 % 10 + '0');
+if(num1 != 98 || num2 != 99)
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
