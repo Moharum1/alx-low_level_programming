cbb946298e16f04ac13b6d19e8aa954af64330bb
Moharum1 <moharum4@gmail.com>
2023-03-18 08:38:36 -0700

    HIII

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,14 @@
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
+#include <stdio.h>
+
+int main(void) {
 int num;
 scanf("%d", &num);
-
-if (num > 0) {
+    if (num > 0) {
         printf("%d is greater than zero\n", num);
-} 
-else if (num < 0) 
-{
+    } else if (num < 0) {
         printf("%d is less than zero\n", num);
-} 
-else
-{
-printf("%d is equal to zero\n", num);
+    } else {
+        printf("%d is equal to zero\n", num);
+    }
+    return (0);
 }
-return (0);
-}
-
