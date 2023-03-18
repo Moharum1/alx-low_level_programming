a9fda23de48dc924d1fefe12fc8b593e825e02b4
Moharum1 <moharum4@gmail.com>
2023-03-18 10:26:13 -0700

    Hi

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ if (num1 != num2)
 {
 putchar(num1 / 10 + '0');
 putchar(num1 % 10 + '0');
-putchar(' '); 
+putchar(' ');
 putchar(num2 / 10 + '0');
 putchar(num2 % 10 + '0');
 if (num1 != 98 || num2 != 99)
