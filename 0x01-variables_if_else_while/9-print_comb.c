764794d7e745edf99f13bc6faeed14bd3f8da464
Moharum1 <moharum4@gmail.com>
2023-03-18 09:57:26 -0700

    Hi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,10 @@ int main(void)
 int i = 0;
 while (i < 10) 
 {
-int j = 0;
-while (j < 10) 
-{
 putchar(i + '0');
-putchar(j + '0');
-j++;
-}
+putchar(' ');
+putchar(',');
+putchar(' ');
 i++;
 }
 return (0);
