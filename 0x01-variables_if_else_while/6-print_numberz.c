a46f22dac7fe9b54a33ddef8cc6dde8c62b99287
Moharum1 <moharum4@gmail.com>
2023-03-18 09:41:36 -0700

    hi

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch = 0;
+while (ch < 10)
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
