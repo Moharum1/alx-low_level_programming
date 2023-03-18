125c32d4ad61d89ca4216bd30621127304f79ae0
Moharum1 <moharum4@gmail.com>
2023-03-18 09:47:27 -0700

    hi

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = '0';
+while (ch <= '9')
+{
+putchar(ch);
+ch++;
+}
+while (ch <= 'f')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
