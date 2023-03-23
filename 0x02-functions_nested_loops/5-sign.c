994f45577f343cb84dd298ad21827ebc0413f7b0
Moharum1 <moharum4@gmail.com>
2023-03-23 14:27:35 -0700

    HIO

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * print_sign - a function that checks for sign of input
+ * @c: Number
+ * Return: + if int c is postive, - if c is negative , 0 otherwizei
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (c < 0)
+	{
+		printf("-");
+		return (-1);
+	}
+	else
+	{
+		printf(0);
+		return (0);
+	}
+}
