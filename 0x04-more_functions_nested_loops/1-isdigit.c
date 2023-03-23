845dadf179ce79c10af6aabd2145ae8d614c7726
Moharum1 <moharum4@gmail.com>
2023-03-23 16:23:03 -0700

    Hi

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * _isdigit - a function to check if input isa digit between 0 and 9
+ * @c: A Number
+ * Return: if 1 if yes and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 1 && c <= 9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
