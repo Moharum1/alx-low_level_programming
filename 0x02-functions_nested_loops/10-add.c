2a2287b1c1537683eb9032b1ccb987f78e174e95
Moharum1 <moharum4@gmail.com>
2023-03-23 15:13:22 -0700

    hi

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,9 +4,9 @@
  * add - a function that return sum of two numbers
  * @a : first number
  * @b : second number
- * Return: 0 
+ * Return: 0
  */
-int add(int a , int b)
+int add(int a, int b)
 {
-    return (a + b);
+return (a + b);
 }
