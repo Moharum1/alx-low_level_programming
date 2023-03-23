561f097c0a922cedb44ccc7dca7b23ce28642386
Moharum1 <moharum4@gmail.com>
2023-03-23 03:20:26 -0700

    HI

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,17 +4,6 @@
  *
  * Return : All the alphabets
  */
-void print_alphabet(void);
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
 void print_alphabet(void)
 {
 char start = 'a';
