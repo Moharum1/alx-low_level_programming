a1961285db0136993ab7f5f0b9fd769976ce2c8c
Moharum1 <moharum4@gmail.com>
2023-03-23 16:17:22 -0700

    hi

---
+++ 0-isupper.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * _isupper - a function to check if input is upper case
+ * @c: A Number
+ * Return: if c is upper or not
+ */
+int _isupper(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return (1);
+    }
+    else 
+    {
+        return (0);
+    }
+}
