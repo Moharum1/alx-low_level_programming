06246d57b1b962c2fad6fc585bd52c38c17cd6f4
Moharum1 <moharum4@gmail.com>
2023-03-23 16:36:03 -0700

    Hi

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * print_numbers - print all the number from 1 to 9
+ * 
+ * Return: All numbers from 1 to 9
+ */
+void print_numbers(void)
+{
+    for (int i = 0; i <= 9; i++)
+    {
+	    printf("%d\n",i);
+    }	    
+}
