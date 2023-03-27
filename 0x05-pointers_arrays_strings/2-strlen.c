3ce22c32a9f3069508400fec3b1cef4dd9c9953e
Moharum1 <moharum4@.com>
2023-03-27 22:08:38 +0200

    Hi

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - Get the lenght of a String
+ * @s : String 
+ * Return: lenght of the String S
+ */
+int _strlen(char *s)
+{
+	int len;
+	while(*s != '\0')
+	{
+		l++;
+            	s++;
+        }
+    	return (len);
+}
