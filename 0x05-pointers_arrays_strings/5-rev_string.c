6399da86215f63953c262b941f00b7d391ef6d82
Moharum1 <moharum4@.com>
2023-03-27 23:13:19 +0200

    Hi

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,15 +6,12 @@
  * @str : String we want to print
  * Return: None
  */
-void rev_string(char* str)
+void rev_string(char *str)
 {
 int l, i=0;
 char *begin_ptr, *end_ptr, ch;
- 
 l = strlen(str);
- 
 begin_ptr = str;
-   
 end_ptr = str + l - 1;
 
 while (i < (l - 1) / 2) {
@@ -22,7 +19,6 @@ i++;
 ch = *end_ptr;
 *end_ptr = *begin_ptr;
 *begin_ptr = ch;
- 
 begin_ptr++;
 end_ptr--;
 }
