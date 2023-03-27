8fca798ee34ae274d5359e42815d8f9fad19bb37
Moharum1 <moharum4@.com>
2023-03-27 23:07:29 +0200

    Hi

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,27 +11,18 @@ void rev_string(char* str)
     int l, i=0;
     char *begin_ptr, *end_ptr, ch;
  
-    // Get the length of the string
     l = strlen(str);
  
-    // Setting the begin_ptr
-    // to start of string
     begin_ptr = str;
    
-      //Setting the end_ptr the end of
-      //the string
     end_ptr = str + l - 1;
  
-    // Swap the char from start and end
-    // index using begin_ptr and end_ptr
     while (i < (l - 1) / 2) {
         i++;
-        // swap character
         ch = *end_ptr;
         *end_ptr = *begin_ptr;
         *begin_ptr = ch;
  
-        // update pointers positions
         begin_ptr++;
         end_ptr--;
     }
