e5434b5a606331c5d9038f291d1e9918c9d4d8db
Moharum1 <moharum4@.com>
2023-03-27 22:18:36 +0200

    Hi

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - Print a String of Characters
+ * @str : String we want to print
+ * Return: None
+ */
+void _puts(char *str)
+{
+        while(*str != '\0')
+        {
+            printf("%c", *str);
+            str++;
+        }
+}
+
