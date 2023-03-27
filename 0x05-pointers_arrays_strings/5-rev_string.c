afb941f3f7da8c181b64e9e015e59936c60a8509
Moharum1 <moharum4@.com>
2023-03-27 23:09:19 +0200

    Hi

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,22 @@
  */
 void rev_string(char* str)
 {
-    int l, i=0;
-    char *begin_ptr, *end_ptr, ch;
+int l, i=0;
+char *begin_ptr, *end_ptr, ch;
  
-    l = strlen(str);
+l = strlen(str);
  
-    begin_ptr = str;
+begin_ptr = str;
    
-    end_ptr = str + l - 1;
+end_ptr = str + l - 1;
+
+while (i < (l - 1) / 2) {
+i++;
+ch = *end_ptr;
+*end_ptr = *begin_ptr;
+*begin_ptr = ch;
  
-    while (i < (l - 1) / 2) {
-        i++;
-        ch = *end_ptr;
-        *end_ptr = *begin_ptr;
-        *begin_ptr = ch;
- 
-        begin_ptr++;
-        end_ptr--;
-    }
+begin_ptr++;
+end_ptr--;
+}
 }
