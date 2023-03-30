23ea6791c4c6d9965000521cbaa9227d0ebea6d8
Moharum1 <moharum4@.com>
2023-03-30 14:02:04 +0200

    hi

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_strncat - increament a certin number of letter to the destination
+ * @dest : the array we will copy data to
+ * @src : array we will copy data from
+ * @n : number of letter we want to copy
+ * Return: the copied string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *p = dest + strlen(dest);
+	while (i != n) 
+	{
+		*p++ = *src++;
+		i++;
+	}
+	*p = '\0';
+	return dest;
+}
+
