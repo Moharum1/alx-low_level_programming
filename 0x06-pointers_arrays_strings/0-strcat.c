1ef39f6f243f07f09f096ef78a0e4189fe6aa0bd
Moharum1 <moharum4@.com>
2023-03-30 13:56:30 +0200

    Hi

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_strcat - add the content of the second strig to the 1st one 
+ * @dest : the array we will copy data to
+ * @src : array we will copy data from
+ * Return: the copied string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest + strlen(dest);
+	while (*src != '\0')
+	{
+		*p++ = *src++;
+	}
+	*p = '\0';
+	return dest;
+}
