54bf2217f99dd81b2e7fbec08a6a9cea01e1b31e
Moharum1 <moharum4@.com>
2023-04-05 12:04:26 +0200

    done#

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_puts_recursion - a Function that prints a string
+ * @s : the array we will print
+ * Return: nothing
+ */
+char _puts_recursion(char *s)
+{
+	printf("%c",*s);
+	s++;
+	if (*s == '\0')
+	{
+		printf("\n");
+		return;
+	}
+	return (_puts_recursion(s));
+}
