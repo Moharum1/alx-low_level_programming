9ee607c540ee46a29ac89d615153b656071cc3f8
Moharum1 <moharum4@.com>
2023-04-05 13:17:19 +0200

    Hi

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_pow_recursion - return number to the power with respect to other
+ * @x : the number
+ * @y : the power
+ * Return: x**y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > -1 && y <= 1 && y != 0)
+		return (x);
+	if (y == 0)
+		return (1);
+	if (y <= -1)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}
