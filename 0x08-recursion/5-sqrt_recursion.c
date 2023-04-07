d517b77ac9646dc2dcca3cebfea2a63a2f6f984e
Moharum1 <moharum4@.com>
2023-04-07 02:30:50 +0200

    Hi

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,13 +2,20 @@
 #include <string.h>
 #include "main.h"
 
-double sqrt_recursive(double n, double x, double e) {
+/**
+ * *sqrt_recursion - return the square root of a number
+ * @n : the number
+ * Return: sqrt(x)
+ */
+double sqrt_recursive(double n, double x, double e) 
+{
 	double root = (x + n / x) / 2;
-	if (x - root < e) 
+
+	if (x - root < e)
 	{
-		return root;
+		return (root);
 	}
-	return sqrt_recursive(n, root, e);
+	return (sqrt_recursive(n, root, e));
 }
 
 /**
@@ -16,9 +23,10 @@ double sqrt_recursive(double n, double x, double e) {
  * @n : the number
  * Return: sqrt(x)
  */
- int _sqrt_recursion(int n)
+int _sqrt_recursion(int n)
 {
 	double x = n;
 	double e = 0.000001;
-	return sqrt_recursive(n, x, e);
+
+	return (sqrt_recursive(n, x, e));
 }
