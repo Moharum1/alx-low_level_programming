67daf3d175e336ee3b863ba759047d6ab54bf515
Moharram <Moharum4@gmail.com>
2023-04-12 02:43:40 +0200

    hi

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all the arguments it receives
+ * @argv: array of pointers to strings
+ * @argc: argument count
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 1)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		int i = 1;
+		int result = 1;
+
+		while (i < argc)
+		{
+			result = result * atoi(argv[i]);
+			i++;
+		}
+		printf("%d\n", result);
+		return (0);
+	}
+}
