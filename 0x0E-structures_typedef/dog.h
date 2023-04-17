211f03e10fe4a91e25d18486e10297090642a863
Moharram <Moharum4@gmail.com>
2023-04-17 17:10:24 +0200

    hi

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,16 +9,10 @@
  *
  * Description: store different data types
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
+};
 #endif
