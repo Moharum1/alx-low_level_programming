b8da447b193f78b63a050625fcaefe9f823ece0d
Moharram <Moharum4@gmail.com>
2023-04-17 17:44:26 +0200

    hi:

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - function with 1 arg.
+ * Description: print the struct variable
+ * Return: na
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d -> name != NULL)
+		{
+			printf("Name: %s\n", d -> name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+
+		}
+
+		printf("Age: %f\n", d -> age);}
+
+		if (d -> owner != NULL)
+		{
+			printf("Owner: %s\n", d -> owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
+	}
+}
