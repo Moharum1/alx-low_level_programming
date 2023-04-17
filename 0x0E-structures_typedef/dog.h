85f566217e8b4b5a7937fa41293a80a1a185ebd6
Moharram <Moharum4@gmail.com>
2023-04-17 17:05:49 +0200

    ho

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef Dog
 #define Dog
 
-struct dog
+/**
+ * struct dog - struct with dog info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: store different data types
+ */
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+}dog_t;
 
 #endif
