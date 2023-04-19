ccc1fa3c4a50e31f53e18ed7788c6f3b5d8400cc
Moharram <Moharum4@gmail.com>
2023-04-19 14:12:45 +0200

    hi

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - check if the number equal a comparator or not
+ * @size: number of elements in an array
+ * @array : array containing the elements we want to compare
+ * @cmp : pointer to the compare function
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !size || !cmp)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+	
+}
