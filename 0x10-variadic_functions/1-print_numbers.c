10a7f4555fe4d8529f9efe17a2872f88a9845106
Moharram <Moharum4@gmail.com>
2023-04-21 18:03:56 +0200

    hi

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - print all the number separated by the a character
+ * @n: Number of arguments;i
+ * @separator: the string we use for separation
+ *
+ * Return: sum of the numbers.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+
+	va_start(ap, n);
+
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(ap);
+}
