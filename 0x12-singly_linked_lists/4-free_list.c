5a925f0ef55a89b7d74db8901a27f8345ca35783
Moharram <Moharum4@gmail.com>
2023-04-28 04:35:20 +0300

    done

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,14 +2,14 @@
 
 /**
  * free_list - remove the list.
- * @h: header of the linked list;
+ * @head: header of the linked list;
  *
  * Return: nothing.
  */
 void free_list(list_t *head)
 {
 	list_t *tmp;
-	
+
 	while (head)
 	{
 		tmp = head->next;
