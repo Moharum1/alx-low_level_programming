dcd25a1e36affcb90e566f9eed4c30f0e6330f55
Moharum1 <moharum4@.com>
2023-05-09 01:47:56 +0200

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	len = 0;
+
+	if (text_content == NULL)
+		return (-1);
+
 	len = strlen(text_content);
 
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
@@ -28,5 +33,5 @@ int create_file(const char *filename, char *text_content)
 
 	close(fp);
 
-	return (w);
+	return (1);
 }
