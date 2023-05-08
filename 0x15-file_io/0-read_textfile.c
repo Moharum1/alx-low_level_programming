6eba359018675ee4b6c4aa288f8549d9d5d18d14
Moharum1 <moharum4@.com>
2023-05-09 01:17:29 +0200

    done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/*
+ * read_textfile - a function used to read and print the content of a txt file
+ * @filename: name of the file
+ * @letters : no of char in the file 
+ *
+ * Return: the actual number of letter it could read or print
+ */
+size_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fp;
+	ssize_t content,w;
+
+	char *text;
+
+	if (filename == NULL)
+		return (0);
+
+	fp = open(filename,O_RDONLY);
+	if (!fp)
+		return (0);
+
+	text = malloc(sizeof(char)*letters);
+	content = read(fp, text, letters);
+	w = write(STDOUT_FILENO, text, content);
+
+	free(text);
+	close(fp);
+
+	return (w);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef _main_
+#define _main_
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif
