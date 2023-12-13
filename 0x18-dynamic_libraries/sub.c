7ee2d5d7447c5053b972a68f6d90631342bd5630
Moharum1 <moharum4@gamil.com>
2023-12-13 13:31:39 +0200

    task done

--- 0x18-dynamic_libraries/sub.c
+++ 0x18-dynamic_libraries/sub.c
@@ -1,10 +1,24 @@
-int _putchar(char c){}
-int _islower(int c){}
-int _isalpha(int c){}
-int _abs(int n){}
-int _isupper(int c){}
-int _isdigit(int c){}
-int _strlen(char *s){}
+int _putchar(char c){
+    return c;
+}
+int _islower(int c){
+    return c;
+}
+int _isalpha(int c){
+    return c;
+}
+int _abs(int n){
+    return n;
+}
+int _isupper(int c){
+    return c;
+}
+int _isdigit(int c){
+    return c;
+}
+int _strlen(char *s){
+    return s;
+}
 void _puts(char *s){}
 char *_strcpy(char *dest, char *src){}
 int _atoi(char *s){}
