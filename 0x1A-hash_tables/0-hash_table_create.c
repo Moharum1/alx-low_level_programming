f7d45bf89f7734ff1ff3f48ce452c47f95aeb625
Moharum1 <moharum4@gamil.com>
2023-12-22 15:09:46 +0200

    task done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,34 +8,22 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
 	unsigned long int i;
+	hash_table_t *hashtable;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
+	hashtable = malloc(sizeof(hash_table_t));
+	if (hashtable == NULL)
+		return (NULL);
+	hashtable->size = size;
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+	if (hashtable->array == NULL)
+		return (NULL);
 
-	/*Create a hashTable Var*/
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
-	{
-		return (NULL);
-	}
-
-	/*create a head node*/
-	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (table->array == NULL)
-	{
-		return (NULL);
-	}
 	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
-
-	table->size = size;
-
-	return (table);
+	{
+		hashtable->array[i] = NULL;
+	}
+	return (hashtable);
 }
-
-
-
