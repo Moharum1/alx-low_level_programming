34bf4ca9c09dbe2d2fe5bc1666f7b649f6aabe04
Moharum1 <moharum4@gamil.com>
2023-12-22 16:16:28 +0200

    task done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		currentNode = ht->array[pos];
-		while (currentNode != NULL && strcmp(current->key, key) != 0)
+		while (currentNode != NULL && strcmp(currentNode->key, key) != 0)
 		{
 			currentNode = currentNode->next;
 		}
