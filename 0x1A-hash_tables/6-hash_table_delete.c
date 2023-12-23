437d2a6ede0d042102e87dcec07c69baf740d0f4
Moharum1 <moharum4@gamil.com>
2023-12-23 13:39:17 +0200

    task done

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,4 +1,10 @@
 #include "hash_tables.h"
+/**
+ * hash_table_delete - Delete the hash table.
+ * @ht: Hash table.
+ *
+ * Return: Void.
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *currentNode, *tempNode;
