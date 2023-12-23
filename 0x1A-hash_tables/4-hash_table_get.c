90d98f570848e15787f7484514616f5d396a7027
Moharum1 <moharum4@gamil.com>
2023-12-23 13:13:43 +0200

    task done

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	while (content != NULL && strcmp(content->key, key) != 0)
 	{
-		content = content->next
+		content = content->next;
 	}
 
 	if (content == NULL)
